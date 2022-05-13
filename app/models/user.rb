6d27f7630004a3ded5bbbd09dbf9add0db38aa31
Jérémy Pestelard <jeremy.pestelard@gmail.com>
2022-05-13 10:05:10 +0200

    feat(user): add role + seed for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable
 
+  enum role: { user: 0, admin: 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create user with role 'user'
+User.create(email: 'user@example.com', password: 'fz8rrFUs3d@', password_confirmation: 'fz8rrFUs3d@', role: :user)
+# create user with role 'admin'
+User.create(email: 'admin@example.com', password: 'fz8rrFUs3d@', password_confirmation: 'fz8rrFUs3d@', role: :admin)
