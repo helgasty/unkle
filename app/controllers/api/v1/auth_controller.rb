d3c7a10253697e7df6a606a12215f63c738332cd
Jérémy Pestelard <jeremy.pestelard@gmail.com>
2022-05-13 11:18:56 +0200

    feat(api): add login

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class AuthController < ApplicationController
+
+      include Devise::Controllers::Helpers
+      skip_before_action :authenticate_user!
+
+      def login
+        if params.has_key?(:user) && params[:user].has_key?(:email) && params[:user].has_key?(:password)
+
+          # check user exist
+          user = User.find_by_email(params[:user][:email])
+
+          if user.nil?
+            render json: { status: 'KO', message: t('api.login.login_failed')}, status: 500
+          else
+            if user.valid_password?(params[:user][:password])
+              sign_in(:user, user)
+              render json: { status: 'OK', message: t('api.login.login_success'), token: Jwt.encode(user_id: user.id) },  status: 200
+            else
+              render json: { status: 'KO', message: t('api.login.password_failed')}, status: 403
+            end
+          end
+        else
+          render json: { status: 'KO', message: t('api.login.bad_request')}, status: 500
+        end
+      end
+    end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+
+  delegate :t, to: I18n
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    request.headers['token'].present? ? authenticate_request : super
+  end
+
+  private
+
+  def authenticate_request
+    decoded_auth_token = Jwt.decode(request.headers['token'])
+
+    # check token already valid
+    case decoded_auth_token
+    when 403
+      render json: { status: 'KO', message: t('api.login.jwt_token_expired')}, status: 403
+    when 500
+      render json: { status: 'KO', message: t('api.login.jwt_token_error')}, status: 500
+    else
+      @current_user = User.find(decoded_auth_token[:user_id])
+    end
+  end
 end
