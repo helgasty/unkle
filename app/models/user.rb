797d1310d2185fcc6d97fc97b592328fb7ba709b
Jérémy Pestelard <jeremy.pestelard@gmail.com>
2022-05-13 14:32:56 +0200

    feat(device): enable user trackable fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable
+  devise :database_authenticatable, :trackable
 
   has_and_belongs_to_many :contracts
   enum role: [:user, :admin]
