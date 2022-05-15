faf0e86d638cc439d08c8e433f09338db377864d
Jérémy Pestelard <jeremy.pestelard@gmail.com>
2022-05-16 00:47:34 +0200

    feat(subscription): add scope and custom validate

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :contract
   belongs_to :user
+
+  scope :valid_subscriptions, -> { where("end_subscription_at is null OR end_subscription_at > #{Date.today}") }
+  validate :validate_subscription_date
+
+  private
+
+  def validate_subscription_date
+    if end_subscription_at < subscribed_at
+      errors.add('vadidate_subscription_date', I18n.t('api.subscriptions.unsubscribe.date_error'))
+    end
+  end
 end
